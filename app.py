26bfb2c289abe1e11b118bd734e002ace5f13ec1
Luan Fernandes <souluanf@icloud.com>
2023-03-05 16:10:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
+csrf = CSRFProtect(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Laboratório Pipeline DevOps'
 
 
 if __name__ == '__main__':
