26f71110f01f5ee7052c6cffae642f75a6b66f90
Luan Fernandes <souluanf@icloud.com>
2023-03-05 16:13:18 -0300

    Update test.py

--- test.py
+++ test.py
@@ -11,4 +11,4 @@ class Test(unittest.TestCase):
         self.assertEqual(self.result.status_code, 200)
 
     def test_content(self):
-        self.assertEqual(self.result.data.decode('utf-8'), 'Hello World')
+        self.assertEqual(self.result.data.decode('utf-8'), 'Laboratório Pipeline DevOps')
